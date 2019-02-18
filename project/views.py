7089ea2416123df05134aa73da5416c601fd7c70
us <us@email.address>
2019-02-18 19:16:30 +0100

    Added error handler for registration process. Unit tests.

--- project/views.py
+++ project/views.py
@@ -4,6 +4,7 @@ from functools import wraps
 from flask import Flask, flash, redirect, render_template, request, session, url_for
 from flask_sqlalchemy import SQLAlchemy
 from forms import AddTaskForm, RegisterForm, LoginForm
+from sqlalchemy.exc import IntegrityError
 
 __author__ = 'kot'
 
@@ -31,7 +32,8 @@ def login_required(test):
 
 # route handlers
 
-@app.route('/logout')
+@app.route('/logout/')
+@login_required
 def logout():
     session.pop('logged_in', None)
     session.pop('user_id', None)
@@ -114,10 +116,14 @@ def register():
     if request.method == 'POST':
         if form.validate_on_submit():
             new_user = User(form.name.data, form.email.data, form.password.data)
-            db.session.add(new_user)
-            db.session.commit()
-            flash('Thanks for registering. Please login.')
-            return redirect(url_for('login'))
+            try:
+                db.session.add(new_user)
+                db.session.commit()
+                flash('Thanks for registering. Please login.')
+                return redirect(url_for('login'))
+            except IntegrityError:
+                error = 'That username and/or email already exist.'
+                return render_template('register.html', form=form, error=error)
     return render_template('register.html', form=form, error=error)
 
 
