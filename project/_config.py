cd0c1570591d8fff6a28ffd0b7dc4ed9fbb40b8b
us <us@email.address>
2019-04-09 20:46:16 +0200

    SQLite was returned.

--- project/_config.py
+++ project/_config.py
@@ -15,4 +15,4 @@ SECRET_KEY = b'\xcfD\xe6,%\xda\x90\n\x89?X\x01\xfaYJg\x90e\x1f\x84\x12D\xbf\x03\
 DATABASE_PATH = os.path.join(basedir, DATABASE)
 
 # database uri
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///' + DATABASE_PATH)
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH  # os.environ.get('DATABASE_URL', 'sqlite:///' + DATABASE_PATH)
