acd93d97b511bf1a60fb68c0d39568e0023306fb
us <us@email.address>
2019-04-16 21:38:18 +0200

    GET method of API for 'tasks' was created.

--- project/_config.py
+++ project/_config.py
@@ -5,7 +5,7 @@ __author__ = 'kot'
 # grab the folder where this script lives
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = False
+DEBUG = True
 
 DATABASE = 'flasktaskr.db'
 WTF_CSRF_ENABLED = True
